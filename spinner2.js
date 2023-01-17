085434fdf2a84dfdb97faa107e019106027ab1f2
Katie Liu <katieliu59@gmail.com>
2023-01-17 17:08:45 -0500

    Refactor Spinner1 code to use setTimeout

---
+++ spinner2.js
@@ -0,0 +1,48 @@
+const characters = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|"];
+let timeoutDelay = 100;
+
+for (const char of characters) {
+	setTimeout(function () {
+		process.stdout.write(`\r${char}`);
+	}, timeoutDelay);
+    timeoutDelay += 200;
+    
+}//REFACTOR SPINNER 1 (code below)
+// setTimeout(() => {
+// 	process.stdout.write("\r|   ");
+// }, 100);
+
+// setTimeout(() => {
+// 	process.stdout.write("\r/   ");
+// }, 300);
+
+// setTimeout(() => {
+// 	process.stdout.write("\r-   ");
+// }, 500);
+
+// setTimeout(() => {
+// 	// Need to escape the backslash since it's a special character.
+// 	process.stdout.write("\r\\   ");
+// }, 700);
+
+// // ... fill in the rest yourself ...
+// setTimeout(() => {
+// 	process.stdout.write("\r|   ");
+// }, 900);
+
+// setTimeout(() => {
+// 	process.stdout.write("\r/   ");
+// }, 1100);
+
+// setTimeout(() => {
+// 	process.stdout.write("\r-   ");
+// }, 1300);
+
+// setTimeout(() => {
+// 	// Need to escape the backslash since it's a special character.
+// 	process.stdout.write("\r\\   ");
+// }, 1500);
+
+// setTimeout(() => {
+// 	process.stdout.write("\r|   ");
+// }, 1700);
